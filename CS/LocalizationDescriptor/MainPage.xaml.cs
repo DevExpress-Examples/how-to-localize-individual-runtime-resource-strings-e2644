6d463b6092fc7065022274a7e223de88d50ed27a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/LocalizationDescriptor/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using DevExpress.Xpf.Grid;
+
+namespace LocalizationDescriptor {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            view.RuntimeLocalizationStrings.Add(new RuntimeStringIdInfo(
+                GridControlRuntimeStringId.GridGroupPanelText,
+                "To group by a column, drag its header here"));
+            List<string> source = new List<string>() { "test string" };
+            grid.DataSource = new ProductList();
+        }
+
+        private void view_Loaded(object sender, RoutedEventArgs e) {
+            grid.View.ShowColumnChooser();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Localize Individual Runtime Resource Strings
+
+
+<p>This example shows how to change the default string displayed within the Group Panel and the Column Chooser's caption, at runtime. To translate individual resource strings at runtime, create a RuntimeStringIdInfo object with the specified Id and Value properties, and add it to the View's RuntimeLocalizationStrings collection. The Id property identifies the required resource string. The Value property specifies a new value for it.</p>
+
+<br/>
+
+
